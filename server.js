c4bbb554a4c3d1b68a5c8e5128ee925d348e335e
mohsinht <23508720+mohsinht@users.noreply.github.com>
2019-06-21 21:22:55 +0500

    Little change

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
