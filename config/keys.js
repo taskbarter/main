334baa3ac1c8aba1b00bb4dbac6fa7005d6f9d24
Mohsin Hayat <23508720+mohsinht@users.noreply.github.com>
2019-07-11 10:02:00 +0500

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://mohsin-hayat:TaskBarterStartup123@taskbarterdb-vu1cm.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"  
+}; 
