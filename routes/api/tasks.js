2c7fe4d0b18ef733768caa2fd8d0b220c787ffc2
mohsinht <23508720+mohsinht@users.noreply.github.com>
2019-07-14 20:57:21 +0500

    added some task APIs

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -27,4 +27,54 @@ router.post('/add', (req, res) => {
     .catch(err => console.log(err));
 });
 
+// @route   GET api/tasks/all/:user_id/:skip/:limit
+// @desc    get all tasks of some specific user.
+// @access  Public
+router.get('/all/:user_id/:skip_tasks/:limit_tasks', async (req, res) => {
+  try {
+    const limit = parseInt(req.params.limit_tasks) || 0;
+    const skip = parseInt(req.params.skip_tasks) || 0;
+
+    const uTask = await Task.find({
+      user_id: req.params.user_id
+    })
+      .limit(limit)
+      .skip(skip);
+
+    if (!uTask) {
+      return res.status(200).json({ msg: 'No tasks found' });
+    }
+    res.json(uTask);
+  } catch (err) {
+    if (err.kind == 'ObjectId') {
+      return res
+        .status(400)
+        .json({ msg: 'Could not find tasks for this user.' });
+    }
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/tasks/:task_id
+// @desc    get all tasks of some specific user.
+// @access  Public
+router.get('/:task_id', async (req, res) => {
+  try {
+    const required_task = await Task.findById(req.params.task_id);
+
+    if (!required_task) {
+      return res.status(400).json({ msg: 'No tasks found' });
+    }
+
+    res.json(required_task);
+  } catch (err) {
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Could not find the specified task' });
+    }
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
