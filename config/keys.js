7a2333b4e02cf3ff7662d17657f1a8f732fd4ff2
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-01-13 15:42:03 +0500

    some bugs resolved

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost/taskbarter', //process.env.MONGO_URI,
+  mongoURI: process.env.MONGO_URI,
   secretOrKey: 'secret',
   jwtSecret: 'mysecret',
   taskBarterGmail: process.env.EMAIL_ADDRESS,
