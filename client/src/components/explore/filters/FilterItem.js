da90b21b00770c8fa12f7e964080a896e2b8b332
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-03-22 19:49:00 +0500

    filter bar in explore

--- client/src/components/explore/filters/FilterItem.js
+++ client/src/components/explore/filters/FilterItem.js
@@ -2,23 +2,37 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 const FilterItem = props => {
+  const getItemList = () => {
+    return props.item.options.map((opt, i) => (
+      <label className='checkbox__label' key={i}>
+        {opt.name}
+        <input
+          data-v-68624b48=''
+          type='checkbox'
+          name='filter-dropdown-experience'
+          value='d98c139f-5a36-4848-9230-02777a668e33'
+        />{' '}
+        <span className='checkmark'></span>
+      </label>
+    ));
+  };
+
+  const itemHtml = <React.Fragment>{getItemList()}</React.Fragment>;
   return (
     <React.Fragment>
       <a
         className='filter-item'
         role='button'
-        id='dropdown04'
-        data-toggle='dropdown'
-        aria-haspopup='true'
-        aria-expanded='false'
+        id={'PopoverFilter_' + props.item.id}
       >
         <div className='filter-title'>
           <div className='title-container'>
             <div className='title-item'>
               {' '}
-              <span>{props.item.title}</span>{' '}
+              <span className='filter-title-text'>{props.item.title}</span>{' '}
               <span>
                 <i className='fas fa-caret-down' />
               </span>
@@ -27,74 +41,34 @@ const FilterItem = props => {
         </div>
       </a>
 
-      <div
-        aria-labelledby='dropdown04'
-        className='dropdown-menu dropdown-menu-right tooltip popover popover--open vue-popover-theme open'
-        style={{
-          visibility: 'visible',
-          position: 'absolute',
-          willChange: 'transform',
-          top: '0px',
-          left: '0px',
-          transform: 'translate3d(144px, 105px, 0px)'
-        }}
+      <UncontrolledPopover
+        trigger='legacy'
+        placement='bottom'
+        target={'PopoverFilter_' + props.item.id}
+        className='popover-dropdown'
+        style={{ minWidth: '220px' }}
       >
-        <div className='wrapper'>
-          <div className='popover-inner' style={{ position: 'relative' }}>
-            <div>
-              <div>
-                <div className='popover-menu control-bar__menu popover-menu__filter'>
-                  <div data-v-68624b48='' className='filter-dropdown'>
-                    <div
-                      data-v-68624b48=''
-                      className='filter-dropdown__options'
-                    >
-                      {' '}
-                      <label data-v-68624b48='' className='checkbox__label'>
-                        Backend
-                        <input
-                          data-v-68624b48=''
-                          type='checkbox'
-                          name='filter-dropdown-experience'
-                          value='d98c139f-5a36-4848-9230-02777a668e33'
-                        />{' '}
-                        <span data-v-68624b48='' className='checkmark'></span>
-                      </label>
-                    </div>{' '}
-                    <hr data-v-68624b48='' className='filter__hr' />{' '}
-                    <div data-v-68624b48='' className='filter-dropdown__footer'>
-                      <span
-                        data-v-68624b48=''
-                        className='filter-dropdown__button filter-dropdown__button--destructive'
-                      >
-                        Clear filter
-                      </span>{' '}
-                      <span
-                        data-v-68624b48=''
-                        className='filter-dropdown__button'
-                      >
-                        Apply
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>{' '}
-              <div data-v-b329ee4c='' tabindex='-1' className='resize-observer'>
-                <object
-                  aria-hidden='true'
-                  tabindex='-1'
-                  type='text/html'
-                  data='about:blank'
-                ></object>
-              </div>
-            </div>{' '}
+        <PopoverBody>
+          <div className='filter-dropdown-container'>
             <div
-              className='tooltip-arrow popover-arrow'
-              style={{ left: '46px' }}
-            ></div>
+              data-v-68624b48=''
+              className='filter-dropdown__options filter-list'
+            >
+              {' '}
+              {itemHtml}
+            </div>{' '}
+            <hr style={{ marginBottom: '5px' }} />
+            <div className='dropdown-footer'>
+              <button className='filter-dropdown__button filter-dropdown__button--destructive'>
+                Clear filter
+              </button>
+              <button className='filter-dropdown__button filter-dropdown__button--apply'>
+                Apply
+              </button>
+            </div>
           </div>
-        </div>
-      </div>
+        </PopoverBody>
+      </UncontrolledPopover>
     </React.Fragment>
   );
 };
--- client/src/components/explore/filters/FilterMenu.js
+++ client/src/components/explore/filters/FilterMenu.js
@@ -3,16 +3,50 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import FilterItem from './FilterItem';
+import { Input } from 'reactstrap';
 
 const FilterMenu = props => {
   const item1 = {
     title: 'Skills',
-    options: ['ReactJS', 'Wordpress', 'VueJS', 'NodeJS', 'MongoDB']
+    id: 1,
+    options: [
+      { name: 'ReactJS' },
+      { name: 'Wordpress' },
+      { name: 'VueJS' },
+      { name: 'NodeJS' },
+      { name: 'MongoDB' },
+      { name: 'Firebase' },
+      { name: 'Angular' },
+      { name: 'Redux' }
+    ]
+  };
+
+  const item2 = {
+    title: 'Location',
+    id: 2,
+    options: [
+      { name: 'Lahore, Pakistan' },
+      { name: 'New York, US' },
+      { name: 'Mumbai, India' },
+      { name: 'Karachi, Pakistan' },
+      { name: 'Rome, Italy' },
+      { name: 'Paris, France' },
+      { name: 'Washington DC, US' },
+      { name: 'London, UK' }
+    ]
   };
   return (
     <div className='filter-bar'>
       <div className='filter-container'>
         <FilterItem item={item1} />
+        <FilterItem item={item2} />
+
+        <Input type='select' className='sort-dropdown'>
+          <option>Recommended</option>
+          <option>Newest</option>
+          <option>Saved</option>
+          <option>Applied</option>
+        </Input>
       </div>
     </div>
   );
