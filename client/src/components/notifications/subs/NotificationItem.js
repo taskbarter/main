b74502b9316ddb057920d17ccdd35b87443ab9b5
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-04-09 14:40:15 +0500

    notification UI setup

--- client/src/components/notifications/subs/NotificationItem.js
+++ client/src/components/notifications/subs/NotificationItem.js
@@ -6,7 +6,7 @@ const NotificationItem = props => {
   return (
     <React.Fragment>
       <Link to={props.notif.link_to}>
-        <div className='notification-item'>
+        <div className='notification-item '>
           <div className='notif-text'>{props.notif.text}</div>
           <div className='notif-time'>{moment(props.notif.time).fromNow()}</div>
         </div>
