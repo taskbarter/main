56134fcbe58f0e00feb56f4344d8cefb650b1570
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-04-22 13:17:23 +0500

    profile will now have social links default

--- models/PersonalDetails.js
+++ models/PersonalDetails.js
@@ -138,18 +138,23 @@ const PersonalDetails = new mongoose.Schema({
   social: {
     youtube: {
       type: String,
+      default: '',
     },
     twitter: {
       type: String,
+      default: '',
     },
     facebook: {
       type: String,
+      default: '',
     },
     linkedin: {
       type: String,
+      default: '',
     },
     instagram: {
       type: String,
+      default: '',
     },
   },
   tagline: {
