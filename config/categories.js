ebfeabdfeba6f0dcf2dff767ce1e8e0fa78b8bcb
Mohsin Hayat <23508720+mohsinht@users.noreply.github.com>
2020-05-02 23:40:04 +0500

    Add Skills and Categories

---
+++ config/categories.js
@@ -0,0 +1,79 @@
+const categories = [
+  {
+    name: 'A/B Testing',
+  },
+  {
+    name: 'Accounting',
+  },
+  {
+    name: 'Art & Illustration',
+  },
+  {
+    name: 'Computer Programming',
+  },
+  {
+    name: 'Content Writing',
+  },
+  {
+    name: 'Data Entry',
+  },
+  {
+    name: 'Data Science',
+  },
+  {
+    name: 'Financial Planning',
+  },
+  {
+    name: 'Gaming',
+  },
+  {
+    name: 'Graphic Designing',
+  },
+  {
+    name: 'Human Resources',
+  },
+  {
+    name: 'IT & Networking',
+  },
+  {
+    name: 'Online Marketing',
+  },
+  {
+    name: 'Photography',
+  },
+  {
+    name: 'Project Management',
+  },
+  {
+    name: 'Quality Assurance',
+  },
+  {
+    name: 'Research Work',
+  },
+  {
+    name: 'Resume/Cover Letter',
+  },
+  {
+    name: 'Search Engine Optimization',
+  },
+  {
+    name: 'Technical Support',
+  },
+  {
+    name: 'Transcription',
+  },
+  {
+    name: 'Translation',
+  },
+  {
+    name: 'Video Editing',
+  },
+  {
+    name: 'Voice Talent',
+  },
+  {
+    name: 'Other',
+  },
+];
+
+export default categories;
