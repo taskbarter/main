9fb9c74a93876d757a30192a0d8c5879fa7a0629
Mohsin Hayat <23508720+mohsinht@users.noreply.github.com>
2020-05-02 23:44:10 +0500

    Add Skills and Categories File

---
+++ client/src/config/skills.js
@@ -0,0 +1,245 @@
+const skills = [
+  {
+    name: 'AWS',
+    id: 0,
+  },
+  {
+    name: 'Accounting Software',
+    id: 1,
+  },
+  {
+    name: 'Adobe Illustrator',
+    id: 2,
+  },
+  {
+    name: 'Adobe Photoshop',
+    id: 3,
+  },
+  {
+    name: 'Android',
+    id: 4,
+  },
+  {
+    name: 'B2B Marketing',
+    id: 5,
+  },
+  {
+    name: 'Big Data',
+    id: 6,
+  },
+  {
+    name: 'Brand management',
+    id: 7,
+  },
+  {
+    name: 'Cloud Management',
+    id: 8,
+  },
+  {
+    name: 'Copywriting',
+    id: 9,
+  },
+  {
+    name: 'Cordova',
+    id: 10,
+  },
+  {
+    name: 'Creativity',
+    id: 11,
+  },
+  {
+    name: 'Data Analysis',
+    id: 12,
+  },
+  {
+    name: 'Data Visualization',
+    id: 13,
+  },
+  {
+    name: 'Django',
+    id: 14,
+  },
+  {
+    name: 'Email Marketing',
+    id: 15,
+  },
+  {
+    name: 'Firebase',
+    id: 16,
+  },
+  {
+    name: 'Flutter',
+    id: 17,
+  },
+  {
+    name: 'Front-end Development',
+    id: 18,
+  },
+  {
+    name: 'Git',
+    id: 19,
+  },
+  {
+    name: 'Google Cloud',
+    id: 20,
+  },
+  {
+    name: 'Google Webmaster Tools',
+    id: 21,
+  },
+  {
+    name: 'HTML & CSS',
+    id: 22,
+  },
+  {
+    name: 'Infographics',
+    id: 23,
+  },
+  {
+    name: 'iOS',
+    id: 24,
+  },
+  {
+    name: 'JSON',
+    id: 25,
+  },
+  {
+    name: 'Javascript',
+    id: 26,
+  },
+  {
+    name: 'Jquery',
+    id: 27,
+  },
+  {
+    name: 'Machine learning',
+    id: 28,
+  },
+  {
+    name: 'Mathematics',
+    id: 29,
+  },
+  {
+    name: 'Matlab',
+    id: 30,
+  },
+  {
+    name: 'Microsoft Excel',
+    id: 31,
+  },
+  {
+    name: 'Microsoft Powerpoint',
+    id: 32,
+  },
+  {
+    name: 'Mobile Development',
+    id: 33,
+  },
+  {
+    name: 'MongoDB',
+    id: 34,
+  },
+  {
+    name: 'MySQL',
+    id: 35,
+  },
+  {
+    name: 'Node.js',
+    id: 36,
+  },
+  {
+    name: 'Performance Tracking',
+    id: 37,
+  },
+  {
+    name: 'Php',
+    id: 38,
+  },
+  {
+    name: 'Planning',
+    id: 39,
+  },
+  {
+    name: 'Python',
+    id: 40,
+  },
+  {
+    name: 'Quickbooks',
+    id: 41,
+  },
+  {
+    name: 'React',
+    id: 42,
+  },
+  {
+    name: 'Sales Funnel Management',
+    id: 43,
+  },
+  {
+    name: 'Salesforce',
+    id: 44,
+  },
+  {
+    name: 'Shopify',
+    id: 45,
+  },
+  {
+    name: 'Social media',
+    id: 46,
+  },
+  {
+    name: 'Storyboarding',
+    id: 47,
+  },
+  {
+    name: 'Storytelling',
+    id: 48,
+  },
+  {
+    name: 'Technical writing',
+    id: 49,
+  },
+  {
+    name: 'Tensorflow',
+    id: 50,
+  },
+  {
+    name: 'Troubleshooting',
+    id: 51,
+  },
+  {
+    name: 'User Experience (UX)',
+    id: 52,
+  },
+  {
+    name: 'Video Editing',
+    id: 53,
+  },
+  {
+    name: 'Vuejs',
+    id: 54,
+  },
+  {
+    name: 'Web Design',
+    id: 55,
+  },
+  {
+    name: 'Web Development',
+    id: 56,
+  },
+  {
+    name: 'Wix',
+    id: 57,
+  },
+  {
+    name: 'Wordpress',
+    id: 58,
+  },
+
+  {
+    name: 'Other',
+    id: 59,
+  },
+];
+
+export default skills;
