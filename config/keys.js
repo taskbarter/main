e2956dfef0f8ad912fd63a185a4a710084d0e541
IMDaniyal <daniyalakhlaq1@gmail.com>
2020-05-03 01:09:54 +0500

    Key updated

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost/taskbarter', //For local server localhost
-  // mongoURI: process.env.MONGO_URI, //For live Server taskbarter.com
+  // mongoURI: 'mongodb://localhost/taskbarter', //For local server localhost
+  mongoURI: process.env.MONGO_URI, //For live Server taskbarter.com
   secretOrKey: 'secret', //TODO: Change this before release
   jwtSecret: 'mysecret', //TODO: Change this before release
   taskBarterGmail: process.env.EMAIL_ADDRESS,
