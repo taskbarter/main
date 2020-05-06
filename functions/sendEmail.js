481acda8294e34bab424af8e926bf168a5816a81
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-05-06 18:44:04 +0500

    TEST: email confirmation using sendgrid

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const sgMail = require('@sendgrid/mail');
+const keys = require('../config/keys');
 
 const sendEmail = async (subject, to_email, html, text = '') => {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to: to_email,
-    from: keys.taskBarterGmail,
-    subject: subject,
-    text: text,
-    html: html,
-  };
-  sgMail.send(msg);
+  try {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: to_email,
+      from: keys.taskBarterGmail,
+      subject: subject,
+      text: text,
+      html: html,
+    };
+    sgMail.send(msg);
+  } catch (err) {
+    console.log('EMAIL NOT SENT');
+    console.log(err);
+  }
 };
 
 module.exports = {
