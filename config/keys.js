fdad8e7d57d0e9722404266108d6d6001461788d
IMDaniyal <daniyalakhlaq1@gmail.com>
2020-05-14 19:43:00 +0500

    Fix: Link check updated

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost/taskbarter', //For local server localhost
-  // mongoURI: process.env.MONGO_URI, //For live Server taskbarter.com
+  //mongoURI: 'mongodb://localhost/taskbarter', //For local server localhost
+  mongoURI: process.env.MONGO_URI, //For live Server taskbarter.com
   secretOrKey: 'ZZmmdassadadksmaosdm2143-0ads-asdlkads05tsag', //TODO: Change this before release
   jwtSecret: '65asdg7wq898vcdlknsvdlknalvko3teiyauwtsdklkasdfwq0p9oriubm', //TODO: Change this before release
   taskBarterGmail: process.env.EMAIL_ADDRESS,
