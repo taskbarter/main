caa838ecacbdee5c82c25b31f3a2d5acb21b18fe
mohsinht <23508720+mohsinht@users.noreply.github.com>
2020-05-14 06:33:34 +0500

    Testing email notification

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const sgMail = require('@sendgrid/mail');
 const keys = require('../config/keys');
+const nodemailer = require('nodemailer');
+const generic_template = require('../emails/generic_email');
 
 const sendEmail = async (subject, to_email, html, text = '') => {
   try {
@@ -27,6 +29,45 @@ const sendEmail = async (subject, to_email, html, text = '') => {
   }
 };
 
+const sendEmailUsingNode = async (subject, to_email, text, fname, lname) => {
+  try {
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      tls: {
+        rejectUnauthorized: false,
+      },
+      auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL_ADDRESS,
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+        accessToken: process.env.GMAIL_ACCESS_TOKEN,
+      },
+    });
+
+    var mailOptions = {
+      from: keys.taskBarterGmail,
+      to: to_email,
+      subject: 'Taskbarter | ' + subject,
+      text: text,
+      html: generic_template(fname, lname, to_email, text),
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ');
+      }
+    });
+  } catch (err) {
+    console.log('EMAIL NOT SENT');
+    console.log(err);
+  }
+};
+
 module.exports = {
   sendEmail,
+  sendEmailUsingNode,
 };
